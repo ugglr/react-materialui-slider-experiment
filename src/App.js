8d7dabc29daede4b167dd0448233b671c3d48b33
CarlW <carl@oskarroger.net>
2019-03-08 12:48:25 +0800

    Added a Navbar from Material UI prebuilt components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <p>Hello React!</p>
       </div>
     );
